d7f1384d2d23720dd719e0d3448249e508d866f0
Travis Young <youngtravis424@gmail.com>
2023-04-08 21:28:50 -0400

    Initial Commit

---
+++ H4X0r News/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4X0r News
+//
+//  Created by Travis Young on 4/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0r News/H4X0r_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0r_NewsApp.swift
+//  H4X0r News
+//
+//  Created by Travis Young on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0r_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
